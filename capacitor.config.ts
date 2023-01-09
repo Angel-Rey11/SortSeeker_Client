f659ce683ef84044537ce1c5fce98ed906ee50d9
juanmirivas8 <juanmirivas8@gmail.com>
2023-01-10 00:51:51 +0100

    validacion a medio hacer

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'HelloWorld',
+  appName: 'SortSeeker',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,24 +1,40 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { LoadingController } from '@ionic/angular';
-import { SortAlgorithm } from '../model/SortAlgorithm';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors} from '@angular/forms';
+import {LoadingController} from '@ionic/angular';
+import {SortAlgorithm} from '../model/SortAlgorithm';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit{
+export class Tab1Page implements OnInit {
   ionicForm: FormGroup;
   isSubmitted = false;
-  optionEnum:SortAlgorithm[] = [SortAlgorithm.BUBBLE_SORT,SortAlgorithm.INSERTION_SORT,SortAlgorithm.MERGE_SORT,SortAlgorithm.QUICK_SORT,SortAlgorithm.SELECTOR_SORT,SortAlgorithm.SHELL_SORT]
-  constructor(public formBuilder: FormBuilder,private loadingCtrl: LoadingController) {
+  optionEnum: SortAlgorithm[] = [SortAlgorithm.BUBBLE_SORT, SortAlgorithm.INSERTION_SORT,
+    SortAlgorithm.MERGE_SORT, SortAlgorithm.QUICK_SORT, SortAlgorithm.SELECTOR_SORT, SortAlgorithm.SHELL_SORT];
+
+  constructor(public formBuilder: FormBuilder, private loadingCtrl: LoadingController) {
   }
 
-  ngOnInit() {
+  get minElements() {
+    return this.ionicForm.get('minElements');
+  }
+
+  get maxElements() {
+    return this.ionicForm.get('maxElements');
+  }
+
+  get increment() {
+    return this.ionicForm.get('increment');
+  }
+
+  ngOnInit(): void {
     this.ionicForm = this.formBuilder.group({
-      number: ['', [Validators.required, Validators.pattern('^[0-9]+$')]]
-    })
+      minElements: ['', [Validators.required, Validators.min(1)]],
+      maxElements: ['', [Validators.required, Validators.min(1)]],
+      increment: ['', [Validators.required, Validators.min(1)]],
+    },{validators: this.checkForm});
   }
 
   async submitForm() {
@@ -30,17 +46,14 @@ export class Tab1Page implements OnInit{
         message: 'Realizando ordenamiento ...',
         duration: 2000,
       });
-  
-      loading.present();
+
+      await loading.present();
     }
   }
 
-  get errorControl() {
-    return this.ionicForm.controls;
+  private checkForm(control: AbstractControl): ValidationErrors | null {
+    const minElements = control.get('minElements');
+    const maxElements = control.get('maxElements');
+    return minElements.value > maxElements.value ? {minElementsGreaterThanMaxElements: true} : null;
   }
-
-  async showLoading() {
-    
-  }
-  
 }
